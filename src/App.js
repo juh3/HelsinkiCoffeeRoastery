c69466bf047ee071156496abacc9cda722e52ba4
Juha <juha.tapio20@gmail.com>
2022-10-17 20:09:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Logo from "./components/Logo/Logo";
+import Info from "./components/Info/Info"
+
+function App() {
+  return (
+    <div>
+     <Logo />
+
+     <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Logo.css'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const Logo = () => {
+  return (
+    <div className='logo__container'>
+      <div className = 'wrapper'>
+        <img src = "logo.png" alt = "helsinki coffee roastery logo"/>
+        <h1> Helsinki Coffee Roasters</h1>
+        <p>Because everyone deserves a good cup of tsufe</p>
+      </div>
+
+      <div className = 'wrapper'>
+        <div className='chevron'>
+          <ExpandMoreIcon color = "black" sx={{ fontSize: 110 }} onClick = {() => console.log("jabadabauu")}/>
+        </div>
+      </div>
+     
+    </div>
+  )
+}
+
+export default Logo
