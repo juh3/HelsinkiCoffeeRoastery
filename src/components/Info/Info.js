045f29bc8c1b71a027c10adf6be0de9d8e902f97
Juha <juha.tapio20@gmail.com>
2022-10-17 20:05:09 +0300

    first commit

---
+++ src/components/Info/Info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const jou = () => {
+  return (
+    <div info__container>
+      <div>
+        <p>jou</p>
+        <img src ="roastery_inside.jpg" alt ="Roastery" />
+      </div>
+    
+    </div>
+  )
+}
+
+export default jou
