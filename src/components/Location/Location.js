68166ab479169e7dffc983f4cbdf6d0764507ba7
juh3 <juha.tapio20@gmail.com>
2022-10-25 11:28:26 +0300

    Google map

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Location.css'
-
+import Map from '../Map/Map'
 const Location = () => {
   const openingHours = [
     { day: 'Monday', hours: '8-12' },
@@ -9,6 +9,11 @@ const Location = () => {
     { day: 'Thursday', hours: '8-16' },
     { day: 'Friday', hours: '8-18' },
   ]
+
+  const coords = {
+    center: { lat: 60.19065208536513, lng: 24.949951171875 },
+  }
+
   return (
     <div className="openingHours__container">
       <div className="openingHours__wrapper">
@@ -20,6 +25,7 @@ const Location = () => {
             </li>
           ))}
         </ul>
+        <Map location={coords} zoomLevel={16} />
       </div>
     </div>
   )
